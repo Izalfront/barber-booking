9cf87148cc1297dfcc96fb1b354a53b7a8395850
Muhammad Rizal Wahyudi <128357156+Izalfront@users.noreply.github.com>
2024-10-29 20:10:59 +0800

    wip: service detail page

--- src/routes/Detail-barber/features/Review.svelte
+++ src/routes/Detail-barber/features/Review.svelte
@@ -1,3 +1,98 @@
-<script lang="ts"></script>
+<script lang="ts">
+	import alan from '../img/alan.png';
+	import robert from '../img/robert.png';
+	import sergio from '../img/sergio.png';
+	import miss from '../img/miss.png';
 
-<div>Review feature</div>
+	const StarRating = ({ rating }: { rating: number }) => {
+		const totalStars = 5;
+		const filledStars = Math.floor(rating);
+
+		return Array(totalStars)
+			.fill(null)
+			.map((_, index) => index < filledStars);
+	};
+
+	let review = [
+		{
+			id: 1,
+			title: 'Alan Cartwright',
+			testimonial: 'Good service, recommendation for barber shop seekers',
+			rating: 5.0,
+			img: alan
+		},
+		{
+			id: 2,
+			title: 'Robert Gleichner',
+			testimonial: 'Place is not too far and the shaving results are good',
+			rating: 4.0,
+			img: robert
+		},
+		{
+			id: 3,
+			title: 'Sergio Wilderman',
+			testimonial: 'Price is quite affordable and the service is good, I am very satisfied',
+			rating: 4.0,
+			img: sergio
+		},
+		{
+			id: 4,
+			title: 'Miss Erik VonRueden',
+			testimonial: 'Style of barbering here really follows the times, I like the service',
+			rating: 4.0,
+			img: miss
+		}
+	];
+</script>
+
+<div class="p-6">
+	<h1 class="text-xl font-semibold text-black">Our Service</h1>
+	<div class="mt-6">
+		{#each review as data}
+			<!-- svelte-ignore a11y_click_events_have_key_events -->
+			<!-- svelte-ignore a11y_no_static_element_interactions -->
+			<div
+				class="flex justify-between items-center"
+				onclick={() => {
+					data.id;
+				}}
+			>
+				<div class="flex gap-2 mb-6">
+					<div class="text-start">
+						<img class="size-14" src={data.img} alt="" />
+					</div>
+					<div class="text-start">
+						<h1 class="text-lg font-semibold">{data.title}</h1>
+						<div class="flex items-center gap-x-2">
+							<div class="flex">
+								{#each StarRating({ rating: data.rating }) as isFilled}
+									<svg
+										width="15"
+										height="14"
+										viewBox="0 0 15 14"
+										fill="none"
+										xmlns="http://www.w3.org/2000/svg"
+									>
+										<path
+											d="M5.60227 2.6055C6.44671 1.09067 6.86892 0.333252 7.50016 0.333252C8.1314 0.333252 8.55362 1.09067 9.39805 2.6055L9.61652 2.99741C9.85648 3.42788 9.97646 3.64311 10.1635 3.78512C10.3506 3.92714 10.5836 3.97985 11.0496 4.08528L11.4738 4.18127C13.1136 4.55229 13.9335 4.7378 14.1285 5.36507C14.3236 5.99235 13.7647 6.64597 12.6468 7.9532L12.3576 8.2914C12.0399 8.66288 11.881 8.84861 11.8096 9.0784C11.7381 9.30818 11.7621 9.55599 11.8102 10.0516L11.8539 10.5028C12.0229 12.247 12.1074 13.1191 11.5967 13.5067C11.086 13.8944 10.3184 13.5409 8.78305 12.834L8.38584 12.6511C7.94955 12.4503 7.7314 12.3498 7.50016 12.3498C7.26893 12.3498 7.05078 12.4503 6.61449 12.6511L6.21728 12.834C4.68195 13.5409 3.91428 13.8944 3.4036 13.5067C2.89291 13.1191 2.97742 12.247 3.14643 10.5029L3.19015 10.0516C3.23818 9.55599 3.26219 9.30818 3.19074 9.0784C3.11928 8.84861 2.96045 8.66288 2.64278 8.2914L2.35356 7.9532C1.23566 6.64597 0.676712 5.99235 0.871777 5.36507C1.06684 4.7378 1.88673 4.55229 3.52652 4.18127L3.95075 4.08528C4.41673 3.97985 4.64972 3.92714 4.83679 3.78512C5.02387 3.64311 5.14385 3.42788 5.38381 2.99741L5.60227 2.6055Z"
+											fill={isFilled ? '#FACC15' : '#E5E7EB'}
+										/>
+									</svg>
+								{/each}
+							</div>
+							<p class="text-black">
+								( {data.rating} )
+							</p>
+						</div>
+						<p class="text-slate-500">{data.testimonial}</p>
+					</div>
+				</div>
+			</div>
+		{/each}
+	</div>
+	<div>
+		<button class="w-full h-14 rounded-xl text-white font-semibold bg-[#363062] mt-4 tracking-wide"
+			>Booking Now</button
+		>
+	</div>
+</div>
--- src/routes/Detail-barber/features/Service.svelte
+++ src/routes/Detail-barber/features/Service.svelte
@@ -1,3 +1,79 @@
-<script lang="ts"></script>
+<script lang="ts">
+	import basic from '../img/basic-haircut.png';
+	import kids from '../img/kids-haircut.png';
+	import coloring from '../img/hair-coloring.png';
+	import treatment from '../img/hair-treatment.png';
+	import special from '../img/special-message.png';
 
-<div>Service feature</div>
+	let service = [
+		{
+			id: 'basic-haircut',
+			title: 'Basic Haircut',
+			special: 'Basic haircut & vitamint',
+			salary: 20,
+			img: basic
+		},
+		{
+			id: 'kids-haircut',
+			title: 'Kids Haircut',
+			special: 'Special kids haircut',
+			salary: 15,
+			img: kids
+		},
+		{
+			id: 'hair-coloring',
+			title: 'Hair Coloring',
+			special: 'Hair color treatment',
+			salary: 30,
+			img: coloring
+		},
+		{
+			id: 'hair-treatment',
+			title: 'Hair treatment',
+			special: 'Basic haircut & vitamint',
+			salary: 10,
+			img: treatment
+		},
+		{
+			id: 'special-message',
+			title: 'Special massage',
+			special: 'Additional massage',
+			salary: 10,
+			img: special
+		}
+	];
+</script>
+
+<div class="p-6">
+	<h1 class="text-xl font-semibold text-black">Our Service</h1>
+	<div class="mt-6">
+		{#each service as data}
+			<!-- svelte-ignore a11y_click_events_have_key_events -->
+			<!-- svelte-ignore a11y_no_static_element_interactions -->
+			<div
+				class="flex justify-between items-center"
+				onclick={() => {
+					data.id;
+				}}
+			>
+				<div class="flex gap-2 mb-6">
+					<div class="text-start">
+						<img class="size-14" src={data.img} alt="" />
+					</div>
+					<div class="text-start">
+						<h1 class="text-lg font-semibold">{data.title}</h1>
+						<p class="text-slate-500">{data.special}</p>
+					</div>
+				</div>
+				<div class="flex items-center gap-x-2">
+					<p class="text-black">${data.salary}</p>
+				</div>
+			</div>
+		{/each}
+	</div>
+	<div>
+		<button class="w-full h-14 rounded-xl text-white font-semibold bg-[#363062] mt-4 tracking-wide"
+			>Booking Now</button
+		>
+	</div>
+</div>
