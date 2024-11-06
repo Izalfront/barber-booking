f64c18875c43a64d2238a564d48b9616b79bcfc7
Muhammad Rizal Wahyudi <128357156+Izalfront@users.noreply.github.com>
2024-11-06 16:19:08 +0800

    adjust: deployment

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,5 @@
 <script>
 	import '../app.css';
-	import { Router, Route } from 'svelte-routing';
-	import home from './home/+page.svelte';
-	import barberdetail from './detail-barber/+page.svelte';
 </script>
 
 <div class="app">
